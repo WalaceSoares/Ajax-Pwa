8d13ed35e00342b8e4e2e3a1339d13f5363053d2
Walace Soares <66677030+WalaceSoares@users.noreply.github.com>
2022-06-02 16:50:45 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const url = "https://walacesoares.github.io/Ajax-Pwa"
+const url = "https://walacesoares.github.io/Ajax-Pwa/"
 const CACHE_NAME = "ajax-pwa-v8";
 const assets = [
-    '${url}/',
-    '${url}/index.html',
-    '${url}/manifest.json',
-    '${url}/js/instalar.js',
-    '${url}/js/app.js'
+    '${url}',
+    '${url}index.html',
+    '${url}manifest.json',
+    '${url}js/instalar.js',
+    '${url}js/app.js'
 ];
 
 self.addEventListener('install', (e) => {
@@ -18,13 +18,30 @@ self.addEventListener('install', (e) => {
     );
   });
 
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then(res => {
-            return res || fetch(fetchEvent.request)
-        })
-    )
-})
+// self.addEventListener("fetch", fetchEvent => {
+//     fetchEvent.respondWith(
+//         caches.match(fetchEvent.request).then(res => {
+//             return res || fetch(fetchEvent.request)
+//         })
+//     )
+// })
+
+self.addEventListener('fetch', event => {
+  event.respondWith( // busca conteudo em cache ou retorna resultado da request
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      const requestClone = event.request.clone(); 
+      return fetch(requestClone).then((response) => {        
+        const responseCache = response.clone(); 
+        caches.open(CACHE_NAME).then((cache) => { 
+          cache.put(event.request, responseCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
 
 self.addEventListener('activate', (e) => {
     e.waitUntil(
