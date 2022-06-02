98e5fdaa4144819b8237770114d48a571424dc6d
Walace Soares <66677030+WalaceSoares@users.noreply.github.com>
2022-06-02 16:45:37 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function () {
-      const url = "https://walacesoares.github.io/Ajax-Pwa"
+      const url = "https://walacesoares.github.io/Ajax-Pwa/"
       navigator.serviceWorker
         .register('${url}sw.js')
         .then(res => console.log("service worker registered"))
